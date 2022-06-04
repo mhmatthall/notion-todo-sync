ca5dd94000b2b767e708691088c02899ce8f4a8a
Matt Hall <mhmatthall@yahoo.com>
2022-06-05 00:05:45 +0100

    Add script implementation v1, plus accompanying bash shell script

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+/usr/local/bin/python -m pip install --upgrade pip
+pip install requests
+pip install python-dotenv
+pip install python-dotenv
+/usr/local/bin/python /app/main.py
---
+++ notionsync.py
@@ -0,0 +1,110 @@
+import requests as req
+# For secrets:
+from dotenv import load_dotenv
+import os
+
+def parse_project_origin_name(project_id):
+    """ Request a Project page, get the ID of its Origin property, then request that and parse its plaintext name property
+
+    Args:
+        project_id (str): The ID of the target Project page
+
+    Returns:
+        str: The plaintext name of the page's correct Origin property
+    """
+    # Fetch project page from Notion and extract only its 'Origin' property's page ID
+    origin_id = req.get(
+        f'https://api.notion.com/v1/pages/{project_id}',
+        headers={
+            'Authorization': f"Bearer {os.environ.get('NOTION_INTEGRATION_API_KEY')}",
+            'Notion-Version': '2022-02-22'
+        }
+    ).json().get('properties').get('Origin').get('relation')[0].get('id')
+
+    # Fetch origin page from Notion and extract only the page's plaintext name
+    origin_name = req.get(
+        f'https://api.notion.com/v1/pages/{origin_id}',
+        headers={
+            'Authorization': f"Bearer {os.environ.get('NOTION_INTEGRATION_API_KEY')}",
+            'Notion-Version': '2022-02-22'
+        }
+    ).json().get('properties').get('Name').get('title')[0].get('plain_text')
+
+    return origin_name
+
+
+def set_new_origin_name(page_id, origin_name):
+    """Update the Origin property of a page on Notion
+
+    Args:
+        page_id (str):  The ID of the target page
+        origin_name (str): The new plaintext Origin name
+    """
+    req.patch(
+        f'https://api.notion.com/v1/pages/{page_id}',
+        headers={
+            'Authorization': f"Bearer {os.environ.get('NOTION_INTEGRATION_API_KEY')}",
+            'Notion-Version': '2022-02-22'
+        },
+        # Payload
+        json={
+            "properties": {
+                "Origin": {
+                    "select": {
+                        "name": origin_name
+                    }
+                }
+            }
+        }
+    )
+
+
+def fetch_db(db_id):
+    """Fetch a Notion database
+
+    Returns:
+        dict: The returned database as a JSON-serialisable dict (removes HTTP request guff)
+    """
+    return req.post(
+        f'https://api.notion.com/v1/databases/{db_id}/query',
+        headers={
+            'Authorization': f"Bearer {os.environ.get('NOTION_INTEGRATION_API_KEY')}",
+            'Notion-Version': '2022-02-22'
+        },
+        # Notion filter payload; stops us getting 'Completed' tasks which don't appear in the todo list
+        json={
+            "filter": {
+                "property": "Priority",
+                "select": {
+                    "does_not_equal": "Completed"
+                }
+            }
+        }
+    ).json()['results']
+
+
+def main():
+    # Fetch the to-do list database from Notion
+    db = fetch_db(os.environ.get('NOTION_TARGET_DB_ID'))
+
+    for page in db:
+        # If the page has a relation
+        if page.get('properties').get('Project').get('relation'):
+            project_id = page.get('properties').get('Project').get('relation')[0].get('id')
+            current_origin_name = page.get('properties').get('Origin').get('select')
+
+            # Fetch the correct origin name
+            origin_name = parse_project_origin_name(project_id)
+
+            # If the page's plaintext origin name doesn't match the project's plaintext origin name
+            # OR the page has no plaintext origin name
+            if (current_origin_name != origin_name or not current_origin_name):
+                set_new_origin_name(page.get('id'), origin_name)
+
+
+if __name__ == '__main__':
+    # Import environment vars from ./.env
+    load_dotenv()
+
+    # Less go baybee
+    main()
