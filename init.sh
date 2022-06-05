0fb296c80958e695e4d0e5ba714146b2d4e289cd
Matt Hall <mhmatthall@yahoo.com>
2022-06-05 01:52:06 +0100

    Fix init script not executing when apt cache missing

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+apt-get update
 /usr/local/bin/python -m pip install --upgrade pip
 pip install requests
 pip install python-dotenv
-pip install python-dotenv
+
 /usr/local/bin/python /app/notionsync.py
