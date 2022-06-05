15bd5932e103f85d7d15d5a01f5b4721790a6e97
Matt <42594962+mhmatthall@users.noreply.github.com>
2022-06-05 02:05:38 +0100

    Add comments to init

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# I'm using notion-todo-sync inside a docker container so it needs some setup
+#     I'm using the official Python docker image
+#     I've mapped /app to the repo containing the code on the server
+#
+# Also, my server requires extra faff to get dockerfiles going so I'm just using this nasty shell script -- I don't care :))
+
 apt-get update
 /usr/local/bin/python -m pip install --upgrade pip
 pip install requests
